217c0fd6ecbf50dd0837ca2687acd00183d39e84
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-10-02 20:19:28 +0700

    besok integrasi dengan redux thunk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Form from "./components/Form";
 import Read from "./components/Read";
+import FormRegister from "./components/FormRegister";
+import FormLogin from "./components/FormLogin";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Form} />
         <Route exact path="/read" component={Read} />
+        <Route exact path="/register" component={FormRegister} />
+        <Route exact path="/login" component={FormLogin} />
       </Switch>
     </Router>
   );
---
+++ src/components/FormRegister.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Col, Container } from 'react-bootstrap';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+import './form.css';
+
+function FormRegister() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+
+    const handleSubmit =  (e) => {
+        e.preventDefault()
+        if( email !== "" && password !== "") {
+            const auth = getAuth();
+            createUserWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log('success: ', user);
+                // ...
+              })
+              .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log('error: ', errorCode, errorMessage);
+                // ..
+              });
+            setError(false);
+        } else {
+            setError(true)
+        }
+        
+    } 
+    return (
+        <Container>
+            <Row>
+                <Col sm={12}>
+                    <div className="form-add-content">
+                        <form className='form' >
+                            <h1>
+                            Form Register
+                            </h1>
+                            {error && <h2>Isi cukk!!!</h2>}
+                            <div className='form-inputs'>
+                                {/* <label className='form-label'> Email </label> */}
+                                <input
+                                    className='form-input'
+                                    type='email'
+                                    name='email'
+                                    placeholder='Enter your email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />  
+                                <br/>
+                                          
+                            </div>
+                            <div className='form-inputs'>
+                                {/* <label className='form-label'> Password </label> */}
+                                <input
+                                    className='form-input'
+                                    type='password'
+                                    name='password'
+                                    placeholder='Enter your password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                /> 
+                                <br/>
+                            </div>
+                            <button type='submit' className='form-input-submit' onClick={handleSubmit}>
+                                Sign Up
+                            </button>
+                        </form>  
+                    </div>
+
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default FormRegister
